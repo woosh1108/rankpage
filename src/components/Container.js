6717741d32232f7a55d83b793c6788e08bd31e62
우상희 <woosh1108@naver.com>
2023-12-16 00:06:14 +0900

    자주 묻는 질문 페이지

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,13 +6,14 @@ import RankPage from './RankPage';
 import Nav from './Nav';
 import NoticeDetail from './NoticeDetail';
 import InspectionCriteria from './InspectionCriteria';
+import Question from './Question';
 import './Container.css';
 // <RankPage title="남성 신발 인기 순위" />
 const Container = () => {
   return (
     <div className="container">
       <Nav/>
-      <InspectionCriteria/>
+      <Question/>
     </div>
   );
 };
